c8018f7705f34de0c8dcc9020154f6eda0924c0a
Thibaut Chiron <chiron.thibaut@sfr.fr>
2017-02-06 14:21:00 +0100

    Ajout de paramètre à la fonction avec une vérification de ces paramètres, et de valeur par défaut si ceux-ci sont erroné ou non défini

--- README.txt
+++ README.txt
@@ -9,9 +9,38 @@ Installation :
 
 Utilisation :
 	- Pour faire appel à la fonction un bouton doit être créé avec l'attribut :
-	onclick="generateur()"
-	Le bouton peut être n'importe où dans le formulaire, la logique voudrait qu'il soit à côté de l'input du mot de passe.
+		onclick="generateur()"
+		Le bouton peut être n'importe où dans le formulaire, la logique voudrait qu'il soit à côté de l'input du mot de passe.
+		La fonction peut avoir plusieurs arguments (jusqu'à 4 max) :
+			Le premier argument doit être un tableau contenant 2 autres tableaux au minimum. Ceux-ci doit contenir les caracteres utiles à la création d'un mot de passe.
+				Exemple :
+					MonTableauDeCaractères = [
+						[0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+						["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+						["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+					];
+				Par défaut, celui affiché en exemple sera utilisé.
+			Le deuxième argument doit être un tableau contenant 3 messages au minimum.
+				Le premier message sera adressé à l'utilisateur lorsqu'on lui demandera d'entrer un nombre pour la longueur du mot de passe.
+				Le deuxième message sera adressé à l'utilisateur lorsque le nombre entré s'avère ne pas être un nombre et on lui redemande de bien mettre un nombre.
+				Le troisième message sera adressé à l'utilisateur lorsqu'il aura entré un nombre inférieur au nombre de caractère minimum du mot de passe, ou par défaut inférieur ou égal à 0.
+				Le quatrième message sera adressé à l'utilisateur à la fin du programme pour lui suggèrer de noter le mot de passe afin de ne pas l'oublier, ce message n'est pas obligatoire.
+				Exemple :
+					MesMessagesPersonnalisés = [
+						"Entrez le nombre de caratère de votre mot de passe :",
+						"Vous n'avez pas entré un nombre, \nVeuillez entrer le nombre de caratère de votre mot de passe :",
+						"Vous avez entré un nombre inférieur ou égal à 0, \nVeuillez entrer le nombre de caratère de votre mot de passe :",
+						"Nous vous conseillons de noter le mot de passe sur un papier pour ne pas l'oublier."
+					];
+				Par défaut, celui affiché en exemple sera utilisé.
+			Le troisième argument doit être un nombre. Il définira le nombre de caractère qui s'affichera par défaut lorsque nous demanderons à l'utilisateur d'entrer un nombre pour la longueur de son mot de passe.
+				Par défaut, rien de s'affiche.
+			Le quatrième argument doit être un nombre. Il définira le nombre de caractère minimum que devra composé le mot de passe.
+				Exemple :
+					NombreMinimumDeCaractère = 1;
+				Par défaut, celui affiché en exemple sera utilisé. Ainsi, il ne peut avoir de demande de mot de passe de zéro caractère.
+			Si aucun des arguments n'est passé en paramètre ou si ceux-ci sont erroné, les valeurs par défaut citées seront alors utilisé.
 	- Puis rechercher la ligne suivante dans le fichier .js :
-	window.document.exemple_formulaire.mdp.value
+		window.document.exemple_formulaire.mdp.value
 		Remplacer "exemple_formulaire" par la valeur de l'attribut name de votre formulaire
 		Remplacer "mdp" par la valeur de l'attribut name de votre input qui doit contenir le mot de passe
--- mdp_generator.js
+++ mdp_generator.js
@@ -1,58 +1,111 @@
-/*
-Fonction pour générer un mot de passe
-*/
-function generateur() {
+function generateur(caracteres, messages, nb_caractere_defaut, nb_caractere_min) {
 	/*
-	Déclaration des variables
-	caracteres : tableau contenant
-	- un tableau de tous les chiffres entier, de 0 à 9, pouvant servir à générer un mot de passe
-	- un tableau de toutes les lettres minuscules, de l'alphabet latin, pouvant servir à générer un mot de passe
-	- un tableau de toutes les lettres majuscules, de l'alphabet latin, pouvant servir à générer un mot de passe
-	mot_de_passe : variable qui contiendra le mot de passe final que l'on retournera dans le formulaire
-	messages : tableau contenant les messages à afficher à l'utilisateur
-	nb_caractere_mdp : variable qui contiendra un nombre donné par l'utilisateur, ce nombre servira à générer un mot de passe de cette longueur
+	Paramètre de la fonction
+		caracteres : doit être un tableau, contenant au minimum 2 autres tableaux des caractères pouvant servir à faire un mot de passe
+		messages : doit être un tableau, contenant les messages à afficher à l'utilisateur, 3 messages au minimum
+		nb_caractere_defaut : doit être un nombre, servant de nombre par défaut pour la longueur du mot de passe
+		nb_caractere_min : doit être un nombre, servant de limite inférieure de nombre de caractère pour le mot de passe
+	
+	Déclaration des variables par défaut
+		caracteres_defaut : tableau contenant
+			- un tableau de tous les chiffres entier, de 0 à 9, pouvant servir à générer un mot de passe
+			- un tableau de toutes les lettres minuscules, de l'alphabet latin, pouvant servir à générer un mot de passe
+			- un tableau de toutes les lettres majuscules, de l'alphabet latin, pouvant servir à générer un mot de passe
+		messages_defaut : tableau contenant les messages à afficher à l'utilisateur
+	Ces variables seront utilisé si les paramètres entrés dans la fonction generateur sont erroné ou non défini
 	*/
-	var caracteres = [
+	var caracteres_defaut = [
 		[0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
 		["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
 		["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 	];
-	var mot_de_passe = "";
-	var messages = [
+	var messages_defaut = [
 		"Entrez le nombre de caratère de votre mot de passe :",
 		"Vous n'avez pas entré un nombre, \nVeuillez entrer le nombre de caratère de votre mot de passe :",
 		"Vous avez entré un nombre inférieur ou égal à 0, \nVeuillez entrer le nombre de caratère de votre mot de passe :",
 		"Nous vous conseillons de noter le mot de passe sur un papier pour ne pas l'oublier."
 	];
-	var nb_caractere_mdp = acquisition_nombre_caractere(messages[0]);
+	
+	/*
+	Vérification des paramètres fournis
+	D'abord que caracteres soit bien un tableau
+	Et qu'il contient bien 2 tableaux au minimum
+	*/
+	if (Array.isArray(caracteres) === true && caracteres.length >= 1) {
+		for (var i = 0; i < caracteres.length; i++) {
+			if (Array.isArray(caracteres[i]) === false || caracteres[i] === []) {
+				i = caracteres.length;
+				caracteres = caracteres_defaut;
+			}
+		}
+	}
+	else {
+		caracteres = caracteres_defaut;
+	}
+	
+	/*
+	Ensuite que messages soit bien un tableau
+	Et qu'il contient bien 3 messages au minimum
+	*/
+	if (Array.isArray(messages) === true && messages.length < 2) {
+		for (var i = 0; i < messages.length; i++) {
+			if (typeof messages[i] !== "string" || messages[i] === "") {
+				messages[i] = messages_defaut[i];
+			}
+		}
+	}
+	else {
+		messages = messages_defaut;
+	}
+	
+	/*
+	Après que nb_caractere_defaut soit bien un nombre (ajout d'une exeption pour NaN qui est détecté comme un nombre par typeof)
+	Sinon nous réinitialisons la variable
+	*/
+	if (typeof nb_caractere_defaut !== "number" || Number.isNaN(nb_caractere_defaut) === true) {
+		nb_caractere_defaut = ""; // Ainsi, elle ne mettra aucune valeur
+	}
+	
+	/*
+	Puis que nb_caractere_min soit bien un nombre (ajout d'une exeption pour NaN qui est détecté comme un nombre par typeof)
+	Sinon nous mettrons une valeur par défaut
+	*/
+	if (typeof nb_caractere_min !== "number" || Number.isNaN(nb_caractere_min) === true) {
+		nb_caractere_min = 1; // Ainsi, il n'y aura pas de demande de mot de passe de 0 caractère
+	}
+	
+	/*
+	Déclaration de la variable
+		nb_caractere_mdp : variable qui contiendra un nombre donné par l'utilisateur, ce nombre servira à générer un mot de passe de cette longueur
+	*/
+	var nb_caractere_mdp = acquisition_nombre_caractere(messages[0], nb_caractere_defaut);
 	
 	//	Vérification que nb_caractere_mdp est bien un nombre et supérieur à 0
-	while (Number.isNaN(nb_caractere_mdp) === true || nb_caractere_mdp <= 0) {
+	while (Number.isNaN(nb_caractere_mdp) === true || nb_caractere_mdp < nb_caractere_min) {
 		if (Number.isNaN(nb_caractere_mdp) === true) {
-			nb_caractere_mdp = acquisition_nombre_caractere(messages[1]);
+			nb_caractere_mdp = acquisition_nombre_caractere(messages[1], nb_caractere_defaut);
 		}
-		else if (nb_caractere_mdp <= 0) {
-			nb_caractere_mdp = acquisition_nombre_caractere(messages[2]);
+		else if (nb_caractere_mdp < nb_caractere_min) {
+			nb_caractere_mdp = acquisition_nombre_caractere(messages[2], nb_caractere_defaut);
 		}
 	}
 	
 	// Répartition du nombre de caractère du mot de passe équitable entre les différents types de caractère (entier, lettre minuscule et majuscule)
 	var nb_chaque_caractere = Math.trunc(nb_caractere_mdp / caracteres.length);
-	var caracteres_mdp = [];
 	var nb_chaque_caracteres = [];
-	switch (nb_caractere_mdp % caracteres.length) {
-		case 0 :
-			nb_chaque_caracteres = [nb_chaque_caractere, nb_chaque_caractere, nb_chaque_caractere];
-			break;
-		case 1 :
-			nb_chaque_caracteres = [nb_chaque_caractere + 1, nb_chaque_caractere, nb_chaque_caractere];
-		break;
-		case 2 :
-			nb_chaque_caracteres = [nb_chaque_caractere + 1, nb_chaque_caractere + 1, nb_chaque_caractere];
-		break;
+	var quotient = nb_caractere_mdp % caracteres.length;
+	for (var i = 0; i < caracteres.length; i++) {
+		if (quotient !== 0) {
+			nb_chaque_caracteres.push(nb_chaque_caractere + 1);
+			quotient--;
+		}
+		else {
+			nb_chaque_caracteres.push(nb_chaque_caractere);
+		}
 	}
 	
 	// Sélection de caractère de différent type
+	var caracteres_mdp = [];
 	for (var i = 0; i < caracteres.length; i++) {
 		for (var j = 0; j < nb_chaque_caracteres[i]; j++) {
 			caracteres_mdp.push(caractere_aleatoire(caracteres[i]));
@@ -60,7 +113,7 @@ function generateur() {
 	}
 	
 	// Génération du mot de passe à partir des caractères sélectionnés
-	mot_de_passe = generer(caracteres_mdp);
+	var mot_de_passe = generer(caracteres_mdp);
 	
 	/*
 	Retournes le mot de passe généré dans l'input du formulaire d'inscription
@@ -70,13 +123,15 @@ function generateur() {
 	*/
 	window.document.exemple_formulaire.mdp.value = mot_de_passe;
 	
-	// Puis un message qui conseille de noter le mot de passe sur un papier
-	alert(messages[3]);
+	// Puis un message qui conseille de noter le mot de passe sur un papier, si celui est présent dans la variable messages
+	if (messages.length >= 3) {
+		alert(messages[3]);
+	}
 }
 
 /*
 Fonction caractere_aleatoire qui choisi aléatoirement un caractère dans un tableau de caractère
-tab : un tableau contenant tous les caractères pouvant servir à créer un mot de passe
+	tab : un tableau contenant tous les caractères pouvant servir à créer un mot de passe
 La fonction retournera le caractère sélectionné
 */
 function caractere_aleatoire(tab) {
@@ -86,7 +141,7 @@ function caractere_aleatoire(tab) {
 
 /*
 Fonction generer qui mélange les caractères pour en faire un mot de passe
-tab : un tableau contenant tous les caractères servant à créer le mot de passe
+	tab : un tableau contenant tous les caractères servant à créer le mot de passe
 La fonction retournera le mot de passe généré
 */
 function generer(tab) {
@@ -108,9 +163,10 @@ function generer(tab) {
 
 /*
 Fonction acquisition_nombre_caractere qui permettra d'envoyer une boite de dialogue avec un message personnalisé pour récupérer le nombre de caractère maximum du mot de passe que l'on doit générer
-Le message personnalisé sera renseigné en paramètre de la fonction
+	message : message personnalisé du prompt
+	valeur_defaut : valeur par défaut qui sera affiché dans le champ du prompt
 La fonction retournera le nombre saisi par l'utilisateur
 */
-function acquisition_nombre_caractere(message) {
-	return Math.trunc(Number(prompt(message)));
+function acquisition_nombre_caractere(message, valeur_defaut) {
+	return Math.trunc(Number(prompt(message, valeur_defaut)));
 }
