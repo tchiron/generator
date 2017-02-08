426e00712e1322a1bc2caa28c5d70c8be73e6202
Thibaut Chiron <chiron.thibaut@sfr.fr>
2017-02-08 12:31:48 +0100

    Correction d'une erreur sur les conditions du dernier message utilisateur et modification des commentaires du programme

--- mdp_generator.js
+++ mdp_generator.js
@@ -7,7 +7,7 @@ function generateur(caracteres, messages, nb_caractere_min, nb_caractere_max, nb
 		nb_caractere_max : doit être un nombre, servant de limite supérieure de nombre de caractère pour le mot de passe
 		nb_caractere_defaut : doit être un nombre, servant de nombre par défaut pour la longueur du mot de passe
 	
-	Déclaration des variables par défaut
+	Début de déclaration des variables par défaut
 		caracteres_defaut : tableau contenant
 			- un tableau de tous les chiffres entier, de 0 à 9, pouvant servir à générer un mot de passe
 			- un tableau de toutes les lettres minuscules, de l'alphabet latin, pouvant servir à générer un mot de passe
@@ -27,11 +27,15 @@ function generateur(caracteres, messages, nb_caractere_min, nb_caractere_max, nb
 		"Vous avez entré un nombre de caractère trop grand, \nVeuillez entrer le nombre de caractère de votre mot de passe",
 		"Nous vous conseillons de noter le mot de passe sur un papier pour ne pas l'oublier."
 	];
-	
 	/*
-	Vérification des paramètres fournis
-	D'abord que caracteres soit bien un tableau
-	Et qu'il contient bien 2 tableaux au minimum
+	Fin de déclaration des variables par défaut
+	
+	Début de vérification des paramètres de la fonction
+		D'abord que caracteres soit bien un tableau et qu'il contient bien 2 tableaux au minimum
+		Ensuite que messages soit bien un tableau et qu'il contient bien 3 messages au minimum
+		Puis que nb_caractere_min soit bien un nombre (ajout d'une exeption pour NaN qui est détecté comme un nombre par typeof), sinon nous mettrons une valeur par défaut
+		Après que nb_caractere_max soit bien un nombre (ajout d'une exeption pour NaN qui est détecté comme un nombre par typeof) et pas inférieur ou égal au nombre de caractère minimum, sinon nous réinitialisons la variable
+		Enfin que nb_caractere_defaut soit bien un nombre (ajout d'une exeption pour NaN qui est détecté comme un nombre par typeof), sinon nous réinitialisons la variable
 	*/
 	if (Array.isArray(caracteres) === true && caracteres.length > 1) {
 		for (var i = 0; i < caracteres.length; i++) {
@@ -45,10 +49,6 @@ function generateur(caracteres, messages, nb_caractere_min, nb_caractere_max, nb
 		caracteres = caracteres_defaut;
 	}
 	
-	/*
-	Ensuite que messages soit bien un tableau
-	Et qu'il contient bien 3 messages au minimum
-	*/
 	if (Array.isArray(messages) === true && messages.length > 2) {
 		for (var i = 0; i < messages.length; i++) {
 			if (typeof messages[i] !== "string" || messages[i] === "") {
@@ -60,37 +60,28 @@ function generateur(caracteres, messages, nb_caractere_min, nb_caractere_max, nb
 		messages = messages_defaut;
 	}
 	
-	/*
-	Puis que nb_caractere_min soit bien un nombre (ajout d'une exeption pour NaN qui est détecté comme un nombre par typeof)
-	Sinon nous mettrons une valeur par défaut
-	*/
 	if (typeof nb_caractere_min !== "number" || Number.isNaN(nb_caractere_min) === true) {
 		nb_caractere_min = 1; // Ainsi, il n'y aura pas de demande de mot de passe de 0 caractère
 	}
 	
-	/*
-	Enfin que nb_caractere_max soit bien un nombre (ajout d'une exeption pour NaN qui est détecté comme un nombre par typeof) et pas inférieur ou égal au nombre de caractère minimum
-	Sinon nous réinitialisons la variable
-	*/
 	if (typeof nb_caractere_max !== "number" || Number.isNaN(nb_caractere_max) === true || nb_caractere_max <= nb_caractere_min) {
 		nb_caractere_max = ""; // Ainsi, il n'y a pas de nombre maximal
 	}
 	
-	/*
-	Après que nb_caractere_defaut soit bien un nombre (ajout d'une exeption pour NaN qui est détecté comme un nombre par typeof)
-	Sinon nous réinitialisons la variable
-	*/
 	if (typeof nb_caractere_defaut !== "number" || Number.isNaN(nb_caractere_defaut) === true) {
 		nb_caractere_defaut = ""; // Ainsi, elle ne mettra aucune valeur
 	}
-	
 	/*
-	Déclaration de la variable
+	Fin de vérification des paramètres de la fonction
+	
+	Déclaration de la variable nb_caractere_mdp
 		nb_caractere_mdp : variable qui contiendra un nombre donné par l'utilisateur, ce nombre servira à générer un mot de passe de cette longueur
 	*/
 	var nb_caractere_mdp = acquisition_nombre_caractere(messages[0], nb_caractere_defaut);
-	
-	//	Vérification que nb_caractere_mdp est bien un nombre, supérieur ou égal au nombre minimum de caractère et inférieur ou égal au nombre maximum de caractère, si il y a un nombre maximum de caractère
+	/*
+	Début de vérification de la variable nb_caractere_mdp
+		nb_caractere_mdp : doit être un nombre, supérieur ou égal au nombre minimum de caractère et inférieur ou égal au nombre maximum de caractère, si il y a un nombre maximum de caractère
+	*/
 	while (Number.isNaN(nb_caractere_mdp) === true || nb_caractere_mdp < nb_caractere_min || (nb_caractere_max !== "" && nb_caractere_mdp > nb_caractere_max)) {
 		if (Number.isNaN(nb_caractere_mdp) === true) {
 			nb_caractere_mdp = acquisition_nombre_caractere(messages[1], nb_caractere_defaut);
@@ -102,8 +93,11 @@ function generateur(caracteres, messages, nb_caractere_min, nb_caractere_max, nb
 			nb_caractere_mdp = acquisition_nombre_caractere(messages[3], nb_caractere_defaut)
 		}
 	}
+	/*
+	Fin de vérification de la variable
 	
-	// Répartition du nombre de caractère du mot de passe équitable entre les différents types de caractère (entier, lettre minuscule et majuscule)
+	Début de répartition du nombre de caractère du mot de passe équitable entre les différents types de caractère (entier, lettre minuscule et majuscule)
+	*/
 	var nb_chaque_caractere = Math.trunc(nb_caractere_mdp / caracteres.length);
 	var nb_chaque_caracteres = [];
 	var quotient = nb_caractere_mdp % caracteres.length;
@@ -116,28 +110,33 @@ function generateur(caracteres, messages, nb_caractere_min, nb_caractere_max, nb
 			nb_chaque_caracteres.push(nb_chaque_caractere);
 		}
 	}
+	/*
+	Fin de répartition du nombre de caractère
 	
-	// Sélection de caractère de différent type
+	Début de sélection de caractère de différent type
+	*/
 	var caracteres_mdp = [];
 	for (var i = 0; i < caracteres.length; i++) {
 		for (var j = 0; j < nb_chaque_caracteres[i]; j++) {
 			caracteres_mdp.push(caractere_aleatoire(caracteres[i]));
 		}
 	}
+	/*
+	Fin de sélection des caractères
 	
-	// Génération du mot de passe à partir des caractères sélectionnés
+	Génération du mot de passe à partir des caractères sélectionnés
+	*/
 	var mot_de_passe = generer(caracteres_mdp);
-	
 	/*
 	Retournes le mot de passe généré dans l'input du formulaire d'inscription
-	window.document.NomDuFormulaire.NomDeInput.value
-	NomDeInput correspond à la valeur de l'attribut name de l'input de votre mot de passe
-	NomDuFormulaire correspond à la valeur de l'attribut name du formulaire contenant l'input de votre mot de passe
+		window.document.NomDuFormulaire.NomDeInput.value
+		NomDeInput correspond à la valeur de l'attribut name de l'input de votre mot de passe
+		NomDuFormulaire correspond à la valeur de l'attribut name du formulaire contenant l'input de votre mot de passe
 	*/
 	window.document.exemple_formulaire.mdp.value = mot_de_passe;
 	
 	// Puis un message qui conseille de noter le mot de passe sur un papier, si celui est présent dans la variable messages
-	if (messages.length = 5 && typeof messages[4] === "string") {
+	if (messages.length === 5 && typeof messages[4] === "string") {
 		alert(messages[4]);
 	}
 }
